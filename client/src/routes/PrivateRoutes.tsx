4a32a13ba7351e22d0c1f74de94dbbbb3e41185a
dl-little <little.douglas22@gmail.com>
2024-07-17 12:52:00 -0400

    Loading Spinner when loading

--- client/src/routes/PrivateRoutes.tsx
+++ client/src/routes/PrivateRoutes.tsx
@@ -1,12 +1,13 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '../assets/hooks/useAuth';
+import LoadingSpinner from '../assets/components/LoadingSpinner';
 
 const PrivateRoutes = () => {
   //@ts-expect-error We're not providing a default.
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
 
   return (
-    user ? <Outlet /> : <Navigate to={"/login/"} />
+    user ? <Outlet /> : loading ? <LoadingSpinner /> : <Navigate to={"/login/"} />
   )
 }
 
