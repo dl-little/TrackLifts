9fa21b46d84631c0c3604a184bd801eb70dc4556
dl-little <little.douglas22@gmail.com>
2024-07-18 14:25:25 -0400

    Add Logout button to layout

--- client/src/assets/components/Icons.tsx
+++ client/src/assets/components/Icons.tsx
@@ -50,6 +50,19 @@ export const Cog: React.FC = () => {
   );
 };
 
+export const Exit: React.FC = () => {
+  return (
+    <svg
+      viewBox="0 0 512 512"
+      className="dark:fill-blue-500 dark:group-hover:fill-blue-300 fill-blue-300 group-hover:fill-blue-500"
+      height="1.5em"
+      width="1.5em"
+    >
+      <path d="M335.69 272h-161v-32h161V92a12 12 0 00-12-12h-280a12 12 0 00-12 12v328a12 12 0 0012 12h280a12 12 0 0012-12zM419.06 272l-64 64 22.63 22.63L480.31 256 377.69 153.37 355.06 176l64 64h-83.37v32h83.37z" />
+    </svg>
+  );
+};
+
 const Icons = {
   House: <House />,
   Calendar: <Calendar />,
--- client/src/assets/layouts/AuthLayout.tsx
+++ client/src/assets/layouts/AuthLayout.tsx
@@ -1,8 +1,18 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useCallback } from 'react';
 import { Column } from '../components/Layout';
 import NavigationBar from '../components/NavigationBar';
+import { Exit } from '../components/Icons';
+import { useAuth } from '../hooks/useAuth';
 
 const AuthLayout: React.FC<PropsWithChildren> = ({ children }) => {
+  // @ts-expect-error We don't provide a default.
+  const { logOut } = useAuth();
+
+  const handleClick = useCallback(() => {
+    logOut();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Column $justify="space-between" className="relative">
       <Column
@@ -11,7 +21,15 @@ const AuthLayout: React.FC<PropsWithChildren> = ({ children }) => {
         $align="flex-start"
         className="p-4 pb-0"
       >
-        {children}
+        <button
+          onClick={handleClick}
+          title="Sign Out"
+          className="group fixed right-4 top-4"
+        >
+          <Exit />
+          <p className="screen-reader-text">Sign Out</p>
+        </button>
+        <main>{children}</main>
       </Column>
       <NavigationBar />
     </Column>
