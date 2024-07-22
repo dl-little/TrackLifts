fe707ad94b4beeed6427aba63cf739b30dd8c223
dl-little <little.douglas22@gmail.com>
2024-07-22 16:44:15 -0400

    Wrap plus sign in link

--- client/src/routes/Dashboard.tsx
+++ client/src/routes/Dashboard.tsx
@@ -1,9 +1,17 @@
 import { Plus } from '../assets/components/Icons';
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
   return (
     <>
-      <Plus />
+      <Link
+        title="New Workout"
+        to="/create"
+        className="group dark:text-neutral-100 bg-neutral-300 hover:bg-neutral-400 text-neutral-900 dark:bg-neutral-800 dark:hover:bg-neutral-900"
+      >
+        <Plus />
+        <p className="screen-reader-text">Create a new workout</p>
+      </Link>
     </>
   );
 };
